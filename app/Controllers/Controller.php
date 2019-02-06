d7832718791074c6ad993c8dd463d008997081fd
Vladlen Gilyazetdinov <funkylen@gmail.com>
2019-02-06 23:52:16 +0300

    Добавление валидации зодиака

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -4,16 +4,19 @@ namespace App\Controllers;
 
 use App\Classes\Speed;
 use App\Classes\Zodiac;
+use App\Validator;
 use App\View;
 
 
 class Controller
 {
     protected $view;
+    protected $validator;
 
     public function __construct()
     {
         $this->view = new View();
+        $this->validator = new Validator();
     }
 
     public function action($uri)
@@ -30,10 +33,12 @@ class Controller
 
     public function actionSpeed()
     {
+//        $this->validator->speedValidate($_POST['speed']);
+
         if (isset($_POST['speed'])) {
             $speed = new Speed($_POST['speed']);
             $output = [
-                'speed' => $speed->kphToMph()
+                'message' => $speed->kphToMph()
             ];
             return $this->view->render('speed.twig', $output);
         }
@@ -44,12 +49,24 @@ class Controller
     public function actionZodiac()
     {
         if (isset($_POST['zodiac'])) {
-            $zodiac = new Zodiac($_POST['zodiac']);
-            $output = [
-                'zodiac' =>'Ваш знак зодиака "' . $zodiac->determineZodiac() . '"'
-            ];
+
+            $validation = $this->validator->zodiacValidate($_POST['zodiac']);
+
+            if (!$validation) {
+                $output = [
+                    'alert' =>'Введите корректную дату в формате dd.mm.yyyy'
+                ];
+            } else {
+                $zodiac = new Zodiac($_POST['zodiac']);
+
+                $output = [
+                    'message' =>'Ваш знак зодиака "' . $zodiac->determineZodiac() . '"'
+                ];
+            }
+
             return $this->view->render('zodiac.twig', $output);
         }
+
         return $this->view->render('zodiac.twig');
     }
 }
---
+++ app/Validator.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+
+class Validator
+{
+    public function zodiacValidate($date)
+    {
+        list($day, $month, $year) = explode('.', $date);
+
+        $match = preg_match('/^\d{2}\.\d{2}.\d{4}/', $date);
+
+        if ($match === 0) {
+            return false;
+        } elseif ($day < 1 || $day > 31) {
+            return false;
+        } elseif ($month < 1 || $month > 12) {
+            return false;
+        } elseif ($year < 1900 || $year > date('Y')) {
+            return false;
+        }
+
+        return true;
+    }
+}
